539ead94e0dec3510155249e2b08a90a7bf5e5a6
aydinnemati <aydinnemati199888@gmail.com>
2022-01-02 19:03:56 +0330

    RESTapi

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type user struct {
+	Fname string `json:"first name"`
+	Lname string `json:"last name"`
+}
+
+var users = []user{
+	{Fname: "aydin", Lname: "nemati"},
+	{Fname: "khashy", Lname: "mosavi"},
+	{Fname: "ali", Lname: "tavafi"},
+}
+
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
 func main() {
-	fmt.Println("salaaam")
+	router := gin.Default()
+	router.GET("/users", getUsers)
+
+	router.Run("localhost:3000")
 }
